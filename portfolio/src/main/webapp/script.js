00c960a107ca6bbdf8c0dc9a23c6898eba4791f0
isabella <isabella_aus_china@163.com>
2020-07-11 11:35:11 +0800

    Modify content and change comment

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -4,12 +4,7 @@ Date:   2020.07.08
 Email:  isabella_aus_china@outlook.com
 */
 
-// Jump to intro page from start page
+// The page will jump to intro page from start page
 function jumpToIntro() {
-    window.location.href="intro.html";
-}
-
-// Jump to main page
-function back() {
-    window.location.href="index.html";
+    window.location.href="/intro.html";
 }
