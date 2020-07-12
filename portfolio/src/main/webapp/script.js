5ba69d75a689d52bade319f746a8c9a3a09fa86f
isabella <isabella_aus_china@163.com>
2020-07-12 22:05:37 +0800

    Finish comment

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -4,7 +4,42 @@ Date:   2020.07.08
 Email:  isabella_aus_china@outlook.com
 */
 
-// The page will jump to intro page from start page
+window.addEventListener("load", init);
+
+function init() {
+    var path = window.location.pathname;
+    var page = path.split("/").pop();
+    console.log(page);
+    if (page == "comment.html") {
+        console.log("Aloha");
+        getAllComment();
+    }
+}
+
+// The page will jump to intro page
 function jumpToIntro() {
     window.location.href="/intro.html";
+}
+
+// The page will jump to comment page
+function jumpToComment() {
+    window.location.replace("/comment.html");
+}
+
+function getAllComment() {
+    var allCommentElement = document.getElementById("commentCol");
+
+    fetch("/comment").then(response => response.json()).then(comments=> {
+        console.log("ALoha");
+        console.log(comments);
+        for (const [key, value] of Object.entries(comments)) {
+            var divElement = document.createElement("div");
+            divElement.setAttribute("class", "comment alignLeft");
+            
+            var spanElement = document.createElement("span");
+            spanElement.innerText = value;
+            divElement.appendChild(spanElement);
+            allCommentElement.appendChild(divElement);
+        };
+    });
 }
